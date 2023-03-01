de6d1aef504990f6fd125861378c8cf0a6532c85
Koen van den Berg <kvdbgithub@use.startmail.com>
2023-03-01 19:55:42 +0100

    Install and set up Tailwind

---
+++ fylo-dark-theme-landing-page/tailwind.config.cjs
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'fe-dark-blue-1': 'hsl(217, 28%, 15%)',
+        'fe-dark-blue-2': 'hsl(217, 28%, 13%)',
+        'fe-dark-blue-3': 'hsl(216, 53%, 9%)',
+        'fe-dark-blue-3': 'hsl(219, 30%, 18%)',
+        'fe-cyan': 'hsl(176, 68%, 64%)',
+        'fe-blue': 'hsl(198, 60%, 50%)',
+        'fe-red': 'hsl(0, 100%, 63%)',
+      },
+      fontFamily: {
+        'raleway': ['Raleway'],
+        'open-sans': ['"Open Sans"']
+      }
+    },
+  },
+  plugins: [],
+}
