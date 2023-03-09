bbe4331a576242e433f997b0f37f4045b4c7974d
Koen van den Berg <kvdbgithub@use.startmail.com>
2023-03-09 21:20:11 +0100

    Change logo responsivity

--- fylo-dark-theme-landing-page/src/components/Header.tsx
+++ fylo-dark-theme-landing-page/src/components/Header.tsx
@@ -3,7 +3,7 @@ import Navbar from './Navbar';
 export default function Header() {
   return (
     <header className='flex justify-between items-center max-w-[86rem] mx-auto mt-5 sm:mt-[4.5rem] px-4 xsm:px-8 sm:px-12 text-white font-raleway text-sm sm:text-base select-none'>
-      <img src="/logo.svg" alt="The Fylo logo." className="xsm:pr-6 block h-6 xsm:h-8 sm:h-fit pointer-events-none"/>
+      <img src="/logo.svg" alt="The Fylo logo." className="xsm:pr-6 h-8 sm:h-fit pointer-events-none"/>
       <Navbar />
     </header>
   );
