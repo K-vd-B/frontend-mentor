2ffd98354b1fb951594348a15bfb9c15e28284fb
Koen van den Berg <kvdbgithub@use.startmail.com>
2023-03-09 21:17:46 +0100

    Add hero section

--- fylo-dark-theme-landing-page/src/components/HeroSection.tsx
+++ fylo-dark-theme-landing-page/src/components/HeroSection.tsx
@@ -1,7 +1,14 @@
 export default function HeroSection() {
   return (
-    <article>
+    <article className="text-white text-center">
         <img src="illustration-intro.png" alt="An image of two people storing files in a big folder." className="mx-auto p-4 xsm:p-8 sm:p-12 "/>
+        <h1 className="font-raleway font-bold text-2xl xsm:text-4xl sm:text-[2.5rem] max-w-4xl mx-auto px-4 xsm:px-8 sm:px-12 ">All your files in one secure location, accessible anywhere.</h1>
+        <p className="font-open-sans text-sm xsm:text-base sm:text-lg max-w-md sm:max-w-2xl mx-auto px-4 xsm:px-8 sm:px-12 mt-4 xsm:mt-6 sm:mt-8">Fylo stores all your most important files in one secure location. Access them wherever 
+           you need, share and collaborate with friends family, and co-workers.</p>
+        <button className="button-hover-animation font-raleway font-bold rounded-full px-20 py-3 sm:px-24 sm:py-4 mt-4 xsm:mt-6 sm:mt-8">Get Started</button>
     </article>
   )
 }
+
+
+
