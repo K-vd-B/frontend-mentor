b002e4b06a92e1f51b05ed9c4c79b6cbf432fc08
Koen van den Berg <kvdbgithub@use.startmail.com>
2023-03-09 13:29:45 +0100

    Fix typo in tailwind config

--- fylo-dark-theme-landing-page/tailwind.config.cjs
+++ fylo-dark-theme-landing-page/tailwind.config.cjs
@@ -10,7 +10,7 @@ module.exports = {
         'fe-dark-blue-1': 'hsl(217, 28%, 15%)',
         'fe-dark-blue-2': 'hsl(217, 28%, 13%)',
         'fe-dark-blue-3': 'hsl(216, 53%, 9%)',
-        'fe-dark-blue-3': 'hsl(219, 30%, 18%)',
+        'fe-dark-blue-4': 'hsl(219, 30%, 18%)',
         'fe-cyan': 'hsl(176, 68%, 64%)',
         'fe-blue': 'hsl(198, 60%, 50%)',
         'fe-red': 'hsl(0, 100%, 63%)',
