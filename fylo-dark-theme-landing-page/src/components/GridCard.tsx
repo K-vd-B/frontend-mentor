647dd0fe4d5439b70f35f0eb40f24ce0bd09e7c0
Koen van den Berg <kvdbgithub@use.startmail.com>
2023-03-09 22:57:45 +0100

    Style grid section

--- fylo-dark-theme-landing-page/src/components/GridCard.tsx
+++ fylo-dark-theme-landing-page/src/components/GridCard.tsx
@@ -1,9 +1,9 @@
 export default function GridCard({imageURL, imageAlt, header, body}: {imageURL: string, imageAlt:string, header:string, body: string}) {
   return (
-    <div>
-        <img src={imageURL} alt={imageAlt}/>
-        <h2>{header}</h2>
-        <p>{body}</p>
+    <div className="flex flex-col items-center text-white">
+        <img src={imageURL} alt={imageAlt} className="h-[5.5rem] w-auto"/>
+        <h2 className="font-raleway font-bold text-lg mt-7">{header}</h2>
+        <p className="font-open-sans text-center text-sm max-w-sm mt-2">{body}</p>
     </div>
   )
 }
--- fylo-dark-theme-landing-page/src/components/GridSection.tsx
+++ fylo-dark-theme-landing-page/src/components/GridSection.tsx
@@ -1,24 +1,26 @@
-import GridCard from "./GridCard";
+import GridCard from './GridCard';
 
 export default function GridSection() {
   return (
-    <section>
-        <GridCard imageURL="icon-access-anywhere.svg" imageAlt="A drawing of a computer and screen." 
-        header="Access your files, anywhere" body="The ability to use a smartphone, tablet, or 
-        computer to access your account means your files follow you everywhere."/>
+    <section className="bg-fe-dark-blue-2 text-white">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-16 md:gap-24 max-w-5xl mx-auto px-4 xsm:px-8 sm:px-12 pb-20">
+            <GridCard imageURL="icon-access-anywhere.svg" imageAlt="A drawing of a computer and screen." 
+            header="Access your files, anywhere" body="The ability to use a smartphone, tablet, or 
+            computer to access your account means your files follow you everywhere."/>
 
-        <GridCard imageURL="icon-security.svg" imageAlt="A drawing of a shield." 
-        header="Security you can trust" body="2-factor authentication and user-controlled encryption 
-        are just a couple of the security features we allow to help secure your files."/>
+            <GridCard imageURL="icon-security.svg" imageAlt="A drawing of a shield." 
+            header="Security you can trust" body="2-factor authentication and user-controlled encryption 
+            are just a couple of the security features we allow to help secure your files."/>
 
-        <GridCard imageURL="icon-collaboration.svg" imageAlt="A drawing of a clock." 
-        header="Real-time collaboration" body="Securely share files and folders with friends, family 
-        and colleagues for live collaboration. No email attachments required."/>
+            <GridCard imageURL="icon-collaboration.svg" imageAlt="A drawing of a clock." 
+            header="Real-time collaboration" body="Securely share files and folders with friends, family 
+            and colleagues for live collaboration. No email attachments required."/>
 
-        <GridCard imageURL="icon-any-file.svg" imageAlt="A drawing of a folder." 
-        header="Store any type of file" body=" Whether you're sharing holidays photos or work 
-        documents, Fylo has you covered allowing for all file types to be securely stored and 
-        shared."/>
+            <GridCard imageURL="icon-any-file.svg" imageAlt="A drawing of a folder." 
+            header="Store any type of file" body=" Whether you're sharing holidays photos or work 
+            documents, Fylo has you covered allowing for all file types to be securely stored and 
+            shared."/>
+        </div>
     </section>
   )
 }
