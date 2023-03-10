4cb2ae79ea194fcfea0cc1c90fee769ca22dde1a
Koen van den Berg <kvdbgithub@use.startmail.com>
2023-03-10 19:08:09 +0100

    Add and style info section

--- fylo-dark-theme-landing-page/src/App.tsx
+++ fylo-dark-theme-landing-page/src/App.tsx
@@ -2,6 +2,7 @@ import GetAccessSection from './components/GetAccessSection';
 import GridSection from './components/GridSection';
 import Header from './components/Header';
 import HeroSection from './components/HeroSection';
+import InfoSection from './components/InfoSection';
 import QuotesSection from './components/QuotesSection';
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Header />
       <HeroSection />
       <GridSection />
+      <InfoSection />
       <QuotesSection />
       <GetAccessSection />
     </>
---
+++ fylo-dark-theme-landing-page/src/components/InfoSection.tsx
@@ -0,0 +1,26 @@
+export default function InfoSection() {
+  return (
+    <section className="bg-fe-dark-blue-2 flex flex-wrap gap-20 justify-center mx-auto px-4 xsm:px-8 sm:px-12 pt-20 pb-40">
+        <div className="flex flex-wrap lg:flex-nowrap justify-center gap-5 max-w-7xl">
+            <img src="illustration-stay-productive.png" alt="" />
+            <div className="flex flex-col justify-center items-center lg:items-start text-white text-center lg:text-left flex-1 px-4 xsm:px-8 sm:px-12">
+                <h2 className="font-raleway font-bold text-4xl max-w-xs mb-5 mt-10 lg:mt-0">
+                    Stay productive, wherever you are
+                </h2>
+                <p className="font-open-sans max-w-[35rem] mb-3">
+                    Never let location be an issue when accessing your files. Fylo has you covered for
+                    all of your file storage needs.
+                </p>
+                <p className="font-open-sans max-w-[35rem] mb-4">
+                    Securely share files and folders with friends, family and colleagues for live
+                    collaboration. No email attachments required.
+                </p>
+                <div className="flex flex-row justify-center items-center">
+                    <a className="menu-hover-animation text-fe-cyan text-[1.1rem] py-1 mr-2 cursor-pointer">See how Fylo works</a>
+                    <img src="icon-arrow.svg" alt="A arrow symbol." className="h-8 w-8"/>
+                </div>
+            </div>
+        </div>
+    </section>
+  )
+}
