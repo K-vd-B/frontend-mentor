fd77db1f487e3afeda19268ffd4cdf3321764b60
Koen van den Berg <kvdbgithub@use.startmail.com>
2023-03-10 12:17:56 +0100

    Add the wave to the hero section

--- fylo-dark-theme-landing-page/src/components/HeroSection.tsx
+++ fylo-dark-theme-landing-page/src/components/HeroSection.tsx
@@ -1,6 +1,6 @@
 export default function HeroSection() {
   return (
-    <article className="text-white text-center">
+    <article className="hero-section-wave text-white text-center pb-32 md:pb-40">
         <img src="illustration-intro.png" alt="An image of two people storing files in a big folder." className="mx-auto p-4 xsm:p-8 sm:p-12 "/>
         <h1 className="font-raleway font-bold text-2xl xsm:text-4xl sm:text-[2.5rem] max-w-4xl mx-auto px-4 xsm:px-8 sm:px-12 ">All your files in one secure location, accessible anywhere.</h1>
         <p className="font-open-sans text-sm xsm:text-base sm:text-lg max-w-md sm:max-w-2xl mx-auto px-4 xsm:px-8 sm:px-12 mt-4 xsm:mt-6 sm:mt-8">Fylo stores all your most important files in one secure location. Access them wherever 
