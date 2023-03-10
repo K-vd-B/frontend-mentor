db6fb9b8555bd54f76bcec4f3863eb1bad8ea72e
Koen van den Berg <kvdbgithub@use.startmail.com>
2023-03-10 14:31:09 +0100

    Styles quotes section

--- fylo-dark-theme-landing-page/src/components/QuoteCard.tsx
+++ fylo-dark-theme-landing-page/src/components/QuoteCard.tsx
@@ -1,13 +1,13 @@
 export default function QuoteCard({text, personName, personRole, personAvatarURL}: 
                                  {text: string, personName: string, personRole: string, personAvatarURL: string}) {
   return (
-    <div>
-        <p>{text}</p>
-        <div>
-            <img src={personAvatarURL} alt={`A picture of ${personName}.`} />
+    <div className="relative flex flex-col bg-fe-dark-blue-4 max-w-sm rounded-lg shadow-xl px-8 pt-9 pb-6">
+        <p className="font-open-sans text-[0.925rem]">{text}</p>
+        <div className="flex flex-row items-center mt-5">
+            <img src={personAvatarURL} alt={`A picture of ${personName}.`} className="rounded-full h-8 mr-3" />
             <div>
-                <p>{personName}</p>
-                <p>{personRole}</p>
+                <p className="font-raleway font-bold text-xs">{personName}</p>
+                <p className="font-raleway text-xs">{personRole}</p>
             </div>
         </div>
     </div>
--- fylo-dark-theme-landing-page/src/components/QuotesSection.tsx
+++ fylo-dark-theme-landing-page/src/components/QuotesSection.tsx
@@ -2,22 +2,28 @@ import QuoteCard from './QuoteCard';
 
 export default function QuotesSection() {
   return (
-    <div>
-        <QuoteCard text="Fylo has improved our team productivity by an order of magnitude. Since 
-        making the switch our team has become a well-oiled collaboration machine." 
-        personName="Satish Patel" personRole="Founder & CEO, Huddle" 
-        personAvatarURL="profile-1.jpg"/>
+    <section className="bg-fe-dark-blue-2 text-white pb-24 px-4 xsm:px-8 sm:px-12">
+        <div className="grid grid-cols-1 md:grid-cols-3 justify-items-center max-w-7xl gap-10 mx-auto">
 
-        <QuoteCard text="Fylo has improved our team productivity by an order of magnitude. Since 
-        making the switch our team has become a well-oiled collaboration machine." 
-        personName="Bruce McKenzie" personRole="Founder & CEO, Huddle" 
-        personAvatarURL="profile-2.jpg"/>
+            <div className="relative">
+                <img src="bg-quotes.png" alt="A quotation mark." className="absolute -top-8 -left-2"/>
+                <QuoteCard text="Fylo has improved our team productivity by an order of magnitude. Since
+                making the switch our team has become a well-oiled collaboration machine."
+                personName="Satish Patel" personRole="Founder & CEO, Huddle"
+                personAvatarURL="profile-1.jpg"/>
+            </div>
 
-        <QuoteCard text="Fylo has improved our team productivity by an order of magnitude. Since 
-        making the switch our team has become a well-oiled collaboration machine." 
-        personName="Iva Boyd" personRole="Founder & CEO, Huddle" 
-        personAvatarURL="profile-3.jpg"/>
-        
-    </div>
+            <QuoteCard text="Fylo has improved our team productivity by an order of magnitude. Since
+            making the switch our team has become a well-oiled collaboration machine."
+            personName="Bruce McKenzie" personRole="Founder & CEO, Huddle"
+            personAvatarURL="profile-2.jpg"/>
+
+            <QuoteCard text="Fylo has improved our team productivity by an order of magnitude. Since
+            making the switch our team has become a well-oiled collaboration machine."
+            personName="Iva Boyd" personRole="Founder & CEO, Huddle"
+            personAvatarURL="profile-3.jpg"/>
+
+        </div>
+    </section>
   )
 }
